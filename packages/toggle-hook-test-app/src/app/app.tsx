dc0335b30c8d7e613da02e1707430dc6c016fa8c
fn-faisal <faisal.fn154@gmail.com>
2022-11-29 17:49:32 +0500

    basic example

---
+++ packages/toggle-hook-test-app/src/app/app.tsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+import useToggle from 'use-toggle';
+
+export function App() {
+  const toggle = useToggle();
+  
+  return (
+    <>
+      <input type={ toggle.isOpen() ? 'text' : 'password' } />
+      <button 
+        onClick={toggle}>Show/Hide Pass</button>
+    </>
+  );
+}
+
+export default App;
