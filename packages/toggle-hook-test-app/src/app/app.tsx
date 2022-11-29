33e3bcf3dc609e0de9076e50e85dc25ee9ba4b11
fn-faisal <faisal.fn154@gmail.com>
2022-11-29 17:49:59 +0500

    type

--- packages/toggle-hook-test-app/src/app/app.tsx
+++ packages/toggle-hook-test-app/src/app/app.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
-import useToggle from 'use-toggle';
+import useToggle from '@react-augment/use-toggle';
 
 export function App() {
   const toggle = useToggle();
